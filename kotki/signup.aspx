c376677aaf1320c4e85e128f00531176046485de
michallegut <michallegut@wp.pl>
2017-12-19 04:53:27 +0100

    List ASP_1

---
+++ kotki/signup.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="signup.aspx.cs" Inherits="kotki.WebForm1" %>
+<script type="text/javascript">
+    function resetForm() {
+        document.getElementById("registrationForm").reset();
+    }
+</script>
+<script runat="server">
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            wynik.Text = "Login: " + login.Text + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "Hasło: " + password.Text + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "Email: " + email.Text + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "Wiek: " + wiek.Text + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "Liczba kotów: " + kot.Text;
+        }
+    }
+    protected void clear_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/signup.aspx", true);
+    }
+</script>
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<title>Rejestracja/Zmiana danych</title>
+		<meta charset= "UTF-8" >
+		<meta name="keywords" content="kot, koty, koty, kotki, obrazki, smieszne"/>
+		<meta name="description" content="Fajna strona internetowa na 5.5"/>
+		<meta name="author" content="Ludzie sukcesu"/>
+		<link rel="stylesheet" href="/Content/CSS/styles.css">
+		<link rel="stylesheet" href="/Content/CSS/random-styles.css">
+		<link rel="stylesheet" href="/Content/CSS/registerstyles.css">
+	</head>
+	<body class="style">
+		<header class="center">
+			<a href="index.html"><img src="/Content/Images/logo.gif" alt="LOGO"></a>
+		</header>
+		<section class="center border-img">
+			<h2><?php echo $naglowek; ?></h2>
+			<form id="registrationForm"  runat="server" method='POST' style="border:1px solid #ccc">
+				<div class="container">
+					<label><b>Login</b></label>
+                    <asp:TextBox type="text" runat="server" title="Od 3 do 16 znaków. [a-zA-Z] [0-9] _ -" placeholder="Enter Login" id="login"/>
+                    <asp:RequiredFieldValidator ValidationGroup="validation" class="red" runat="server" id="reqLogin" controltovalidate="login" errormessage="Proszę wypełnić to pole" /><br />
+                    <asp:RegularExpressionValidator ValidationGroup="validation" class="red" runat="server" id="rexLogin" controltovalidate="login" validationexpression="[a-zA-Z0-9_-]{3,16}$" errormessage="Od 3 do 16 znaków. [a-zA-Z] [0-9] _ -" /><br />
+                    <label><b>Haslo</b></label>
+					<asp:TextBox type="text" runat="server" title="Od 3 do 16 znaków. [a-zA-Z] [0-9] _ -" placeholder="Enter Password" id="password"/>
+                    <asp:RequiredFieldValidator ValidationGroup="validation" class="red" runat="server" id="reqPassword" controltovalidate="password" errormessage="Proszę wypełnić to pole" /><br />
+                    <asp:RegularExpressionValidator ValidationGroup="validation" class="red" runat="server" id="rexPassowed" controltovalidate="password" validationexpression="[a-zA-Z0-9_-]{5,18}$" errormessage="Od 5 do 18 znaków. [a-zA-Z] [0-9] _ -" /><br />
+					<label><b>Email</b></label>
+                    <asp:TextBox type="email" runat="server" title="Podaj prawidłowy adres email!" placeholder="Enter Email" id="email"/>
+                    <asp:RequiredFieldValidator ValidationGroup="validation" class="red" runat="server" id="reqEmail" controltovalidate="email" errormessage="Proszę wypełnić to pole" /><br />
+                    <asp:RegularExpressionValidator ValidationGroup="validation" class="red" runat="server" id="rexEmail" controltovalidate="email" validationexpression="[^@]+@[^@]+\.[a-zA-Z]{2,6}" errormessage="Podaj prawidłowy adres email!" /><br />
+					<label><b>Wiek</b></label>
+					<asp:TextBox type="number" runat="server" title="Podaj liczbę dodatnią, mniejszą od 100" placeholder="Enter Age" id="wiek"/>
+                    <asp:RequiredFieldValidator ValidationGroup="validation" class="red" runat="server" id="reqWiek" controltovalidate="wiek" errormessage="Podaj dodatnią liczbę" /><br />
+                    <asp:RangeValidator ValidationGroup="validation" class="red" runat="server" id="rngWiek" controltovalidate="wiek" type="Integer" minimumvalue="1" maximumvalue="99" errormessage="Wiek musi mieścić się w 1-99" /><br />
+                    <label><b>Liczba kotów</b></label>
+					<asp:TextBox type="number" runat="server" title="Podaj liczbę dodatnią, czym więcej tym lepiej" placeholder="Enter Number of Cats" id="kot"/>
+                    <asp:RequiredFieldValidator ValidationGroup="validation" class="red" runat="server" id="reqKot" controltovalidate="kot" errormessage="Podaj liczbę dodatnią, czym więcej tym lepiej" /><br />
+                    <asp:CompareValidator ValidationGroup="validation" class="red" runat="server" id="cmpKot" controltovalidate="kot" valuetocompare="0" operator="GreaterThan" type="Integer" errormessage="Jak nie masz kota to elo" /><br />
+					<asp:Label ID="wynik" runat="server" Text=""></asp:Label><br /><br />
+                    <div class="clearfix">
+                        <asp:Button runat="server" OnClick="clear_Click" id="clear" class="cancelbtn" text="Wyczyść" />
+                        <asp:Button runat="server" ValidationGroup="validation" style="background-color:green;" class="cancelbtn" id="submit" text="REJESTRUJ" />
+					</div>
+				</div>
+			</form>
+		</section>
+		<footer class="center">
+			<h4>Made by Drozd and Legut</h4>
+		</footer>
+</body>
+</html>
---
+++ kotki/signup.aspx.designer.cs
@@ -0,0 +1,186 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace kotki {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Kontrolka registrationForm.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm registrationForm;
+        
+        /// <summary>
+        /// Kontrolka login.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox login;
+        
+        /// <summary>
+        /// Kontrolka reqLogin.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqLogin;
+        
+        /// <summary>
+        /// Kontrolka rexLogin.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator rexLogin;
+        
+        /// <summary>
+        /// Kontrolka password.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox password;
+        
+        /// <summary>
+        /// Kontrolka reqPassword.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqPassword;
+        
+        /// <summary>
+        /// Kontrolka rexPassowed.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator rexPassowed;
+        
+        /// <summary>
+        /// Kontrolka email.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+        
+        /// <summary>
+        /// Kontrolka reqEmail.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqEmail;
+        
+        /// <summary>
+        /// Kontrolka rexEmail.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator rexEmail;
+        
+        /// <summary>
+        /// Kontrolka wiek.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox wiek;
+        
+        /// <summary>
+        /// Kontrolka reqWiek.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqWiek;
+        
+        /// <summary>
+        /// Kontrolka rngWiek.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator rngWiek;
+        
+        /// <summary>
+        /// Kontrolka kot.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox kot;
+        
+        /// <summary>
+        /// Kontrolka reqKot.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqKot;
+        
+        /// <summary>
+        /// Kontrolka cmpKot.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator cmpKot;
+        
+        /// <summary>
+        /// Kontrolka wynik.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label wynik;
+        
+        /// <summary>
+        /// Kontrolka clear.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button clear;
+        
+        /// <summary>
+        /// Kontrolka submit.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button submit;
+    }
+}
